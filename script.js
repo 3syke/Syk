cc25da5d1f2c4e61c6e261454b2192ac23a03c0b
3syke <as4742270@gmail.com>
2026-05-05 22:54:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-document.getElementById("sapaButton").addEventListener("click", function() {
-  alert("Selamat anda kena prank");
-});
-document.getElementById("sapaButton2").addEventListener("click", function()
-                                               {alert("gabutz");});
+document.getElementById("sapaButton").addEventListener("click", function() {  
+‎ alert("Selamat anda kena prank");  
+‎  });  
+‎ 
+‎  document.getElementById("sapaButton").addEventListener("click", function() {  
+‎ alert("gabutz");  
+‎  });
