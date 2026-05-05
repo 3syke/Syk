69c3cc5e87abc1ccacc0aa592c7ffff3b057b6c9
3syke <as4742270@gmail.com>
2026-05-05 22:57:56 +0700

    Fix event listener for sapaButton in script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ document.getElementById("sapaButton").addEventListener("click", function() { 
 ‎ alert("Selamat anda kena prank");  
 ‎  });  
 ‎ 
-‎  .getElementById("sapaButton").addEventListener("click", function() {  
+‎ document2.getElementById("sapaButton").addEventListener("click", function() {  
 ‎ alert("gabutz");  
 ‎  });
